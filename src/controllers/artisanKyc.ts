2548c7f79c251f4de1e987e228280e8ff2e6b065
HussainBabatunde <tudemine@gmail.com>
2025-08-27 22:58:25 +0100

    complete app

--- src/controllers/artisanKyc.ts
+++ src/controllers/artisanKyc.ts
@@ -8,23 +8,30 @@ const {User, Kyc} = require("../../models"); // adjust path if needed
 
 export const kycController = async (req: Request, res: Response) => {
   try {
-    const { name, document} = req.body;
+    const { documents} = req.body;
     const user = req.user
 
+    if (!Array.isArray(documents)) {
+      return res.status(400).json({ error: "documents must be an array" });
+    }
 
-  if(!name) return res.status(400).json({message: "Name is required."})
-  if(!document) return res.status(400).json({message: "Document is required."})
+    // Bulk insert into DB
+    // Attach userId to each document
+    const docsWithUser = documents.map(doc => ({
+      ...doc,
+      userId: user?.id
+    }));
 
-    // Create the user (password will be hashed by the hook)
-    const kyc = await Kyc.create({
-      name,
-      userId: user?.id,
-      document
-    });
+    // Bulk insert
+    const savedDocs = await Kyc.bulkCreate(docsWithUser);
 
-
+    // Update only completedKyc
+    await User.update(
+      { completedKyc: true },
+      { where: { id: user?.id } }
+    );
     
-    return res.status(201).json({ message: 'Kyc submitted successfully.', data: kyc });
+    return res.status(201).json({ message: 'Kyc submitted successfully.', data: savedDocs });
   } catch (error: any) {
     console.error("Error kyc controller:", error);
     return res.status(400).json({ message: error.message });
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,25 +3,26 @@ import { OtpService } from "../services/otpService";
 import { Sequelize, where } from "sequelize";
 import { generateToken } from "../utils/jwt";
 import bcrypt from "bcrypt";
-const {User, JobTypes} = require("../../models"); // adjust path if needed
+const { User, JobTypes } = require("../../models"); // adjust path if needed
 
 
 export const loginController = async (req: Request, res: Response) => {
   try {
-  const { phoneNumber, password } = req.body;
-  if(!phoneNumber) return res.status(400).json({message: "Phone Number is required."})
-  if(!password) return res.status(400).json({message: "Password is required."})
+    const { phoneNumber, password } = req.body;
+    if (!phoneNumber) return res.status(400).json({ message: "Phone Number is required." })
+    if (!password) return res.status(400).json({ message: "Password is required." })
     const userInfo = await User.findOne({
-  where: {phoneNumber}
-  })
+      where: { phoneNumber }
+    })
+    
 
-  if(!userInfo) return res.status(404).json({message: "User does not exist."})
+    if (!userInfo) return res.status(404).json({ message: "User does not exist." })
 
     // Compare passwords
     const isMatch = await bcrypt.compare(password, userInfo.password);
-    if(!isMatch) return res.status(400).json({message: "Invalid credentials." })
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials." })
 
-      // Generate access token
+    // Generate access token
     const accessToken = generateToken({
       id: userInfo?.id,
       email: userInfo?.email,
@@ -38,12 +39,13 @@ export const loginController = async (req: Request, res: Response) => {
         email: userInfo.email,
         phoneNumber: userInfo.phoneNumber,
         userType: userInfo.userType,
-    createdAt: userInfo.createdAt,
-    updatedAt: userInfo.updatedAt
+        completedKyc: userInfo?.completedKyc,
+        createdAt: userInfo.createdAt,
+        updatedAt: userInfo.updatedAt
       },
     });
-    } catch (error: any) {
-      console.error("Error login controller:", error);
+  } catch (error: any) {
+    console.error("Error login controller:", error);
     return res.status(500).json({ message: error.message });
   }
 };
@@ -51,19 +53,19 @@ export const loginController = async (req: Request, res: Response) => {
 export const registerController = async (req: Request, res: Response) => {
   try {
     const { email, password, phoneNumber, lastName, firstName, userType, confirmPassword, userJobType } = req.body;
-  if(!email) return res.status(400).json({message: "Email is required."})
-  if(!password) return res.status(400).json({message: "Password is required."})
-  if(!phoneNumber) return res.status(400).json({message: "Phone number is required."})
-  if(!lastName) return res.status(400).json({message: "Last name is required."})
-  if(!firstName) return res.status(400).json({message: "First name is required."})
+    if (!email) return res.status(400).json({ message: "Email is required." })
+    if (!password) return res.status(400).json({ message: "Password is required." })
+    if (!phoneNumber) return res.status(400).json({ message: "Phone number is required." })
+    if (!lastName) return res.status(400).json({ message: "Last name is required." })
+    if (!firstName) return res.status(400).json({ message: "First name is required." })
 
-    if(password != confirmPassword) return res.status(400).json({message: "Passwords do not match."})
+    if (password != confirmPassword) return res.status(400).json({ message: "Passwords do not match." })
     const userInfo = await User.findOne({
-      where: {phoneNumber}
+      where: { phoneNumber }
     })
 
-    if(userInfo){
-      return res.status(400).json({message: "User already exist."})
+    if (userInfo) {
+      return res.status(400).json({ message: "User already exist." })
     }
     // Create the user (password will be hashed by the hook)
     const user = await User.create({
@@ -76,24 +78,26 @@ export const registerController = async (req: Request, res: Response) => {
       userJobType
     });
 
-      // Generate JWT (only include necessary fields)
+    // Generate JWT (only include necessary fields)
     const accessToken = generateToken({
       id: user!.id,
       email: user!.email,
       userType: user!.userType
     });
-    
-    return res.status(201).json({ message: 'User registered', accessToken, data: {
-    id: user.id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
-    phoneNumber: user.phoneNumber,
-    userType: user.userType,
-    userJobType: user.userJobType,
-    createdAt: user.createdAt,
-    updatedAt: user.updatedAt
-  } });
+
+    return res.status(201).json({
+      message: 'User registered', accessToken, data: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        userType: user.userType,
+        userJobType: user.userJobType,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt
+      }
+    });
   } catch (error: any) {
     console.error("Error register controller:", error);
     return res.status(400).json({ message: error.message });
@@ -101,15 +105,15 @@ export const registerController = async (req: Request, res: Response) => {
 };
 
 export const verifyPhoneController = async (req: Request, res: Response) => {
-   try {
+  try {
     // const allUser = await User.findAll()
     // console.log("db: ", allUser);
-  const { phoneNo } = req.body;
-  if(!phoneNo) return res.status(400).json({message: "Phone number is required."})
-  
-    const {sessionId, otp} = await OtpService.generateOtp(phoneNo)
-    return res.status(200).json({message: "Otp sent successfully", sessionId, otp})
-} catch (err) {
+    const { phoneNo } = req.body;
+    if (!phoneNo) return res.status(400).json({ message: "Phone number is required." })
+
+    const { sessionId, otp } = await OtpService.generateOtp(phoneNo)
+    return res.status(200).json({ message: "Otp sent successfully", sessionId, otp })
+  } catch (err) {
     console.error("Error verify phone controller:", err);
     return res.status(500).json({ error: "Internal Server Error" });
   }
@@ -117,51 +121,51 @@ export const verifyPhoneController = async (req: Request, res: Response) => {
 
 
 export const resendOtpController = async (req: Request, res: Response) => {
-   try {
-  const { phoneNo, sessionId } = req.body;
-  if(!phoneNo) return res.status(400).json({message: "Phone number is required."})
-    if(!sessionId) return res.status(400).json({message: "Session ID is required."})
-  
-    const {otp} = await OtpService.resendOtp(phoneNo, sessionId)
-    return res.status(200).json({message: "Otp sent successfully", otp})
-} catch (err) {
+  try {
+    const { phoneNo, sessionId } = req.body;
+    if (!phoneNo) return res.status(400).json({ message: "Phone number is required." })
+    if (!sessionId) return res.status(400).json({ message: "Session ID is required." })
+
+    const { otp } = await OtpService.resendOtp(phoneNo, sessionId)
+    return res.status(200).json({ message: "Otp sent successfully", otp })
+  } catch (err) {
     console.error("Error resend OTP controller:", err);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 export const validateOtpController = async (req: Request, res: Response) => {
-   try {
-  const { phoneNo, sessionId, otp } = req.body;
-  if(!phoneNo) return res.status(400).json({message: "Phone number is required."})
-    if(!sessionId || !otp) return res.status(400).json({message: "Session ID and OTP is required."})
+  try {
+    const { phoneNo, sessionId, otp } = req.body;
+    if (!phoneNo) return res.status(400).json({ message: "Phone number is required." })
+    if (!sessionId || !otp) return res.status(400).json({ message: "Session ID and OTP is required." })
 
-      const isValid = await OtpService.validateOtp(phoneNo, otp, sessionId)
-      // if(isValid != true){
-      if(otp != "1234"){
-        return res.status(400).json({message: isValid})
-      }
-      const userInfo = await User.findOne({
-        where: {phoneNumber: phoneNo}
+    const isValid = await OtpService.validateOtp(phoneNo, otp, sessionId)
+    // if(isValid != true){
+    if (otp != "1234") {
+      return res.status(400).json({ message: isValid })
+    }
+    const userInfo = await User.findOne({
+      where: { phoneNumber: phoneNo }
+    })
+    if (userInfo) {
+      return res.status(200).json({
+        message: "Validation successful.",
+        status: true,
+        data: {
+          id: userInfo?.id,
+          firstName: userInfo?.firstName,
+          lastName: userInfo?.lastName,
+          email: userInfo?.email,
+          phoneNumber: userInfo?.phoneNumber,
+          userType: userInfo?.userType,
+          createdAt: userInfo?.createdAt,
+          updatedAt: userInfo?.updatedAt
+        }
       })
-      if(userInfo){
-        return res.status(200).json({
-          message: "Validation successful.",
-          status: true,
-          data: {
-        id: userInfo?.id,
-        firstName: userInfo?.firstName,
-        lastName: userInfo?.lastName,
-        email: userInfo?.email,
-        phoneNumber: userInfo?.phoneNumber,
-        userType: userInfo?.userType,
-    createdAt: userInfo?.createdAt,
-    updatedAt: userInfo?.updatedAt
-      }
-        })
-      }
-    return res.status(200).json({message: "Validation successful.", status: false})
-} catch (err) {
+    }
+    return res.status(200).json({ message: "Validation successful.", status: false })
+  } catch (err) {
     console.error("Error validate phone:", err);
     return res.status(500).json({ error: "Internal Server Error" });
   }
@@ -171,7 +175,7 @@ export const validateOtpController = async (req: Request, res: Response) => {
 export const artisansUserController = async (req: Request, res: Response) => {
   try {
     const { key } = req.params; // search by key
-    
+
 
     if (!key) {
       return res.status(400).json({ message: "Key is required." });
@@ -180,16 +184,16 @@ export const artisansUserController = async (req: Request, res: Response) => {
     // Find job type by key
     const users = await User.findAll({
       attributes: { exclude: ["password"] },
-  where: Sequelize.where(
-    Sequelize.fn("JSON_CONTAINS", Sequelize.col("userJobType"), JSON.stringify([key])),
-    1
-  )
-});
+      where: Sequelize.where(
+        Sequelize.fn("JSON_CONTAINS", Sequelize.col("userJobType"), JSON.stringify([key])),
+        1
+      )
+    });
 
-const jobType = await JobTypes.findOne({
-  where: {key}
-})
-// console.log(users.map((u: any) => u.toJSON()));
+    const jobType = await JobTypes.findOne({
+      where: { key }
+    })
+    // console.log(users.map((u: any) => u.toJSON()));
 
 
     return res.status(200).json({
